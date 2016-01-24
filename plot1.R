7b5a694d1b7a092be9e9324ecbf574ddaefdfe32
GavinWangchn <gavinwang.usa@gmail.com>
2016-01-24 02:42:55 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,9 @@
+data_full <- read.csv("F:/Rstudio/HW/Exploratory/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+#by Xiaoyi Wang 2016/01/24
